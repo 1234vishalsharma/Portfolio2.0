6a88ee614291d810beb08c0b183ccb714e03bb70
Vishal Sharma <vishalsharma15122003@gmail.com>
2024-08-12 01:51:43 +0530

    Projects updated

--- js/main.js
+++ js/main.js
@@ -14,36 +14,39 @@ function btn_clicked(btn_id){
 
 
 function follow_on_insta(){
-    window.open("https://www.instagram.com/01_vishal_sharma_01/","_blank")
+    window.open("https://www.instagram.com/01_vishal_sharma_01/","_blank");
 }
 function open_projects(project_id){
     if(project_id == "Memo_AI"){
-        window.open("https://github.com/1234vishalsharma/AI-Assistance","_blank")
+        window.open("https://github.com/1234vishalsharma/AI-Assistance","_blank");
     }
     else if(project_id =="paint_app"){
-        window.open("https://github.com/1234vishalsharma/DoPaint","_blank")
+        window.open("https://github.com/1234vishalsharma/DoPaint","_blank");
     }
     else if(project_id == "calc"){
-        window.open("https://github.com/1234vishalsharma/Oasis-Infobyte/tree/main/My_calculator","_blank")
+        window.open("https://github.com/1234vishalsharma/Oasis-Infobyte/tree/main/My_calculator","_blank");
     }
     else if(project_id == "e_comm"){
-        window.open("https://github.com/1234vishalsharma/E_commerce_APP","_blank")
+        window.open("https://github.com/1234vishalsharma/E_commerce_APP","_blank");
     }
     else if(project_id=="temp_cnvrt"){
-        window.open("https://github.com/1234vishalsharma/Bharat-Intern-Task/tree/main/TempCnvrt","_blank")
+        window.open("https://github.com/1234vishalsharma/Bharat-Intern-Task/tree/main/TempCnvrt","_blank");
     }
     else if(project_id == "TTT"){
-        window.open("https://github.com/1234vishalsharma/CodeClause-Internship-Task/tree/main/ZERO_KATA","_blank")
+        window.open("https://github.com/1234vishalsharma/CodeClause-Internship-Task/tree/main/ZERO_KATA","_blank");
     }
     else if(project_id == "todo"){
-        window.open("https://github.com/1234vishalsharma/Oasis-Infobyte/tree/main/Todolist","_blank")
+        window.open("https://github.com/1234vishalsharma/Oasis-Infobyte/tree/main/Todolist","_blank");
     }
     else if(project_id == "FShare"){
-        window.open("https://f-share.netlify.app/","_blank")
+        window.open("https://f-share.netlify.app/","_blank");
     }else if(project_id == 'videoTranscoder'){
         window.open("https://transcoder.vercel.app/" , "_blank");
     }else if(project_id == "RealEstate"){
-        window.open("https://1234vishalsharma.github.io/coming-soon/" , "_blank");
+        window.open("https://real-estste.vercel.app/" , "_blank");
+    }
+    else if(project_id == "stopwatch"){
+        window.open("https://github.com/1234vishalsharma/Oasis-Infobyte/tree/main/StopWatch" , "_blak");
     }
     else{
         window.open('https://github.com/1234vishalsharma' , "_blank");
@@ -68,7 +71,7 @@ function download_resume(){
     // to be implemented
     alert("Press Ok to download resume");
         try{
-            window.location.href = "Resume/Resume.pdf";
+            window.location.href = "Resume/ResumeAI.pdf";
          }catch(e){}
 }
 
